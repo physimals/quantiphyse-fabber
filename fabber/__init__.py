41a6a0ccb5727e7a2649fab54092a96a1204a231
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-04-09 13:44:44 +0100

    Disable tests and widget from release version

--- fabber/__init__.py
+++ fabber/__init__.py
@@ -4,11 +4,7 @@ Copyright (c) 2016-2017 University of Oxford, Martin Craig
 """
 
 from .process import FabberProcess
-from .widget import FabberWidget
-from .tests import FabberWidgetTest
 
 QP_MANIFEST = {
-    "widgets" : [FabberWidget],
-    "widget-tests" : [FabberWidgetTest],
     "processes" : [FabberProcess],
 }
