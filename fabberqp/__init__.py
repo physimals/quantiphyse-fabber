eeb94fbd0ca3c4e705f4eaf0d874c016b5c3c144
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-01-17 11:07:10 +0000

    Advertise bundled core library

--- fabberqp/__init__.py
+++ fabberqp/__init__.py
@@ -2,6 +2,7 @@
 Author: Martin Craig <martin.craig@eng.ox.ac.uk>
 Copyright (c) 2016-2017 University of Oxford, Martin Craig
 """
+from quantiphyse.utils import get_local_shlib
 
 from .process import FabberProcess
 from .widget import FabberModellingWidget, SimData
@@ -11,5 +12,6 @@ QP_MANIFEST = {
     "widgets" : [FabberModellingWidget, SimData],
     "widget-tests" : [FabberWidgetTest],
     "processes" : [FabberProcess],
+    "fabber-corelib" : [get_local_shlib("fabbercore_shared", __file__)],
     "module-dirs" : ["deps",],
 }
